d45ae7c9f5b511f0a867aa3971a90c234c81b8a7
joao0812 <joaoesalmeida@hotmail.com>
2023-05-24 18:16:41 -0300

    Implements find and count objects

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def manipulandoPixelColor(image_color):
     cv2.imshow('Image drawed', image_color_3)
     cv2.waitKey()
 
+
 def drawGeometricFormsText(image_color):
     vermelho = (0, 0, 255)
     verde = (0, 255, 0)
@@ -49,6 +50,7 @@ def drawGeometricFormsText(image_color):
     cv2.imshow('Drawed image', image_color_1)
     cv2.waitKey()
 
+
 def shadowFilterImage(image_color):
     image_color_1 = copy(image_color)
     for y in range(0, image_color_1.shape[0]):
@@ -56,6 +58,7 @@ def shadowFilterImage(image_color):
             image_color_1[y, x] = image_color_1[y, x] // 5
     cv2.imshow("Blur", image_color_1)
     cv2.waitKey()
+
 
 def cropResizeImage(image_color):
     image_color_1 = copy(image_color)
@@ -70,12 +73,14 @@ def cropResizeImage(image_color):
     cv2.imshow("Cropped image", crop)
     cv2.waitKey()
 
+
 def flipImage(image_color):
     image_color_flip = copy(image_color)
     image_color_flip = cv2.flip(image_color, -1)
     cv2.imshow("Flipped image", image_color_flip)
     cv2.imshow("Source image", image_color)
     cv2.waitKey(0)
+
 
 def rotateImage(image_color):
     img_height = image_color.shape[0]  # get height and width
@@ -92,6 +97,7 @@ def rotateImage(image_color):
 
     cv2.waitKey(0)
 
+
 def makeMask(image_color):
     mask = np.zeros(image_color.shape[:2], dtype="uint8")
 
@@ -103,6 +109,7 @@ def makeMask(image_color):
 
     cv2.imshow("Mask Image", image_color_mask)
     cv2.waitKey(0)
+
 
 def colorScales(image_color):
     image_color = image_color[::2, ::2]
@@ -119,6 +126,7 @@ def colorScales(image_color):
 
     cv2.waitKey(0)
 
+
 def splitGrayColorChannels(image_color):
     image_color = image_color[::2, ::2]  # reduce image size
     (blue_channel, green_channel, red_channel) = cv2.split(image_color)
@@ -128,6 +136,7 @@ def splitGrayColorChannels(image_color):
     cv2.imshow("Gree Channel", green_channel)
     cv2.imshow("Bluee Channel", blue_channel)
     cv2.waitKey(0)
+
 
 def splitColorChannels(image_color):
     image_color = image_color[::2, ::2]  # reduce image size
@@ -145,6 +154,7 @@ def splitColorChannels(image_color):
     cv2.imshow("Blue channel image", cv2.merge([blue_channel, zeros, zeros]))
 
     cv2.waitKey(0)
+
 
 def histogramEqualization(image_color):
     image_color = image_color[::2, ::2]
@@ -206,6 +216,7 @@ def histogramEqualization(image_color):
     colorHist(image_color)
     equalizationHist(image_gray)
 
+
 def smoothingImage(image_color):
     image_color = image_color[::3, ::3]
 
@@ -247,6 +258,7 @@ def smoothingImage(image_color):
     cv2.imshow("Bilateral Blur image", bilateral_blur)
     cv2.waitKey(0)
 
+
 def binarizationLimiar(image_color):
     image_gray = cv2.cvtColor(image_color[::2, ::2], cv2.COLOR_BGR2GRAY)
 
@@ -264,60 +276,72 @@ def binarizationLimiar(image_color):
     cv2.imshow("Bin2", bin2)
     cv2.waitKey(0)
 
+
 def adaptativeThres(image_color):
     image_gray = cv2.cvtColor(image_color[::2, ::2], cv2.COLOR_BGR2GRAY)
-    image_blur = cv2.GaussianBlur(image_gray, (7,7), 0)
+    image_blur = cv2.GaussianBlur(image_gray, (7, 7), 0)
 
-    image_thres_mean = cv2.adaptiveThreshold(image_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 5)
-    image_thres_gauss = cv2.adaptiveThreshold(image_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 21, 5)
+    image_thres_mean = cv2.adaptiveThreshold(
+        image_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 5)
+    image_thres_gauss = cv2.adaptiveThreshold(
+        image_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 21, 5)
 
-    image_thres_mean_blur = cv2.adaptiveThreshold(image_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 5)
-    image_thres_gauss_blur = cv2.adaptiveThreshold(image_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 21, 5)
+    image_thres_mean_blur = cv2.adaptiveThreshold(
+        image_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 21, 5)
+    image_thres_gauss_blur = cv2.adaptiveThreshold(
+        image_blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 21, 5)
 
-    cv2.imshow("Source image", image_gray);
-    cv2.imshow("Threshold Mean image", image_thres_mean);
-    cv2.imshow("Threshold Gaussian image", image_thres_gauss);
+    cv2.imshow("Source image", image_gray)
+    cv2.imshow("Threshold Mean image", image_thres_mean)
+    cv2.imshow("Threshold Gaussian image", image_thres_gauss)
 
-    cv2.imshow("Blur Source image", image_blur);
-    cv2.imshow("Blur Threshold Mean image", image_thres_mean_blur);
-    cv2.imshow("Blur Threshold Gaussian image", image_thres_gauss_blur);
+    cv2.imshow("Blur Source image", image_blur)
+    cv2.imshow("Blur Threshold Mean image", image_thres_mean_blur)
+    cv2.imshow("Blur Threshold Gaussian image", image_thres_gauss_blur)
 
     cv2.waitKey(0)
 
+
 def thresOtsu(image_color):
     image_gray = cv2.cvtColor(image_color[::2, ::2], cv2.COLOR_BGR2GRAY)
-    image_blur = cv2.GaussianBlur(image_gray, (7,7), 0)
+    image_blur = cv2.GaussianBlur(image_gray, (7, 7), 0)
 
-    T = mahotas.thresholding.otsu(image_blur) # Otsu
+    T = mahotas.thresholding.otsu(image_blur)  # Otsu
 
     temp = image_gray.copy()
-    temp[temp>T]=255
-    temp[temp<255]=0
+    temp[temp > T] = 255
+    temp[temp < 255] = 0
     temp = cv2.bitwise_not(temp)
 
-    T=mahotas.thresholding.rc(image_blur) # Riddler-Calvard
+    T = mahotas.thresholding.rc(image_blur)  # Riddler-Calvard
     temp2 = image_gray.copy()
-    temp2[temp2>T]=255
-    temp2[temp2<255]=0
+    temp2[temp2 > T] = 255
+    temp2[temp2 < 255] = 0
     temp2 = cv2.bitwise_not(temp2)
 
-    result = np.vstack([np.hstack([image_gray, image_blur]), np.hstack([temp, temp2])])
+    result = np.vstack(
+        [np.hstack([image_gray, image_blur]), np.hstack([temp, temp2])])
 
     cv2.imshow("Source image", image_gray)
     cv2.imshow("Threshold Mean image", result)
 
     cv2.waitKey(0)
 
+
 def SodalEdgeDetection(sudoku):
     #sudoku = sudoku[::2,::2]
 
-    sudoku_blur = cv2.GaussianBlur(sudoku, (3,3), 1.0)
+    sudoku_blur = cv2.GaussianBlur(sudoku, (3, 3), 1.0)
 
-    grad_x = cv2.Sobel(sudoku, cv2.CV_64F, 1, 0, ksize=3, borderType=cv2.BORDER_DEFAULT)
-    grad_y = cv2.Sobel(sudoku, cv2.CV_64F, 0, 1, ksize=3, borderType=cv2.BORDER_DEFAULT)
+    grad_x = cv2.Sobel(sudoku, cv2.CV_64F, 1, 0, ksize=3,
+                       borderType=cv2.BORDER_DEFAULT)
+    grad_y = cv2.Sobel(sudoku, cv2.CV_64F, 0, 1, ksize=3,
+                       borderType=cv2.BORDER_DEFAULT)
 
-    grad_x_blur = cv2.Sobel(sudoku_blur, cv2.CV_64F, 1, 0, ksize=3, borderType=cv2.BORDER_DEFAULT)
-    grad_y_blur = cv2.Sobel(sudoku_blur, cv2.CV_64F, 0, 1, ksize=3, borderType=cv2.BORDER_DEFAULT)
+    grad_x_blur = cv2.Sobel(sudoku_blur, cv2.CV_64F, 1,
+                            0, ksize=3, borderType=cv2.BORDER_DEFAULT)
+    grad_y_blur = cv2.Sobel(sudoku_blur, cv2.CV_64F, 0,
+                            1, ksize=3, borderType=cv2.BORDER_DEFAULT)
 
     grad_x = np.uint8(np.absolute(grad_x))
     grad_y = np.uint8(np.absolute(grad_y))
@@ -331,13 +355,14 @@ def SodalEdgeDetection(sudoku):
 
     result = np.vstack([np.hstack([sudoku, grad_x, grad_y, image_sobel]),
                         np.hstack([sudoku_blur, grad_x_blur, grad_y_blur, image_sobel_blur])])
-    
+
     cv2.imshow("Result", result)
     cv2.waitKey(0)
 
+
 def filtroLapraceEdgeDetection(img):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img_blur = cv2.GaussianBlur(img_gray, (3,3), 1.0)
+    img_blur = cv2.GaussianBlur(img_gray, (3, 3), 1.0)
 
     lap = cv2.Laplacian(img_blur, cv2.CV_64F)
     lap = np.uint8(np.absolute(lap))
@@ -345,21 +370,96 @@ def filtroLapraceEdgeDetection(img):
     cv2.imshow('Filtro Laplaciano', res)
     cv2.waitKey(0)
 
+
 def EdgeDetectionCanny(img):
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     suave = cv2.GaussianBlur(img_gray, (7, 7), 0)
     canny1 = cv2.Canny(suave, 20, 120)
     canny2 = cv2.Canny(suave, 70, 200)
-    resultado = np.vstack([np.hstack([img_gray, suave ]),np.hstack([canny1, canny2])]) 
+    resultado = np.vstack([np.hstack([img_gray, suave]),
+                          np.hstack([canny1, canny2])])
     cv2.imshow("Detector de Bordas Canny", resultado)
     cv2.waitKey(0)
+
+# Função para facilitar a escrita nas imagem
+
+
+def escreve(img, texto, cor=(255, 0, 0)):
+    fonte = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img, texto, (10, 20), fonte, 0.5, cor, 0, cv2.LINE_AA)
+
+
+def identifyAndCountObjects(img):
+    # Passo 1: Conversão para tons de cinza
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Passo 2: Blur/Suavização da imagem
+    img_blur = cv2.blur(img_gray, (5, 5))
+
+    # Passo 3: Binarização resultando em pixels brancos e pretos
+    thres = mahotas.thresholding.otsu(img_blur)
+    print(thres)
+    bin = img_blur.copy()
+    # thres vai no lugar de 245, mas usei esse valor para achar somente os 6 dados da imagem e n seus valores também
+    bin[bin > 245] = 255
+    bin[bin < 255] = 0
+    bin = cv2.bitwise_not(bin)
+
+    # Passo 4: Detecção de bordas com Canny
+    edges = cv2.Canny(bin, 70, 150)
+
+    # Passo 5: Identificação e contagem dos contornos da imagem
+    # cv2.RETR_EXTERNAL = conta apenas os contornos externos
+    contours, objetos = cv2.findContours(
+        edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # A variável lx (lixo) recebe dados que não são utilizados
+
+    # FINDCONTOURS -> É usada no OpenCV para encontrar os contornos em uma imagem.
+    # image: A imagem binarizada de entrada, onde os contornos serão encontrados. Geralmente é uma imagem binária obtida após a aplicação de técnicas de limiarização ou detecção de bordas.
+    # mode: O modo de recuperação dos contornos. Pode ser cv2.RETR_EXTERNAL para retornar apenas os contornos externos, cv2.RETR_LIST para retornar todos os contornos em uma lista simples, cv2.RETR_TREE para retornar todos os contornos em uma hierarquia completa, ou cv2.RETR_CCOMP para retornar todos os contornos em uma hierarquia de dois níveis.
+    # method: O método de aproximação dos contornos. Pode ser cv2.CHAIN_APPROX_NONE para armazenar todos os pontos do contorno, cv2.CHAIN_APPROX_SIMPLE para armazenar apenas os pontos extremos do contorno, cv2.CHAIN_APPROX_TC89_L1 ou cv2.CHAIN_APPROX_TC89_KCOS para usar o algoritmo de aproximante de Freeman para reduzir o número de pontos do contorno.
+    # contours: Uma lista vazia (ou None) que será preenchida com os contornos encontrados. Cada contorno é representado por um array NumPy.
+    # hierarchy: Uma matriz (opcional) para armazenar a hierarquia dos contornos. É usado para representar a relação de aninhamento e ordem entre os contornos.
+    # offset (opcional): Um deslocamento a ser adicionado a todos os pontos dos contornos.
+
+    print(len(contours))
+    print(len(objetos))
+
+    escreve(img_gray, "Imagem em tons de cinza", 0)
+    escreve(img_blur, "Suavizacao com Blur", 0)
+    escreve(bin, "Binarizacao com Metodo Otsu", 255)
+    escreve(edges, "Detector de bordas Canny", 255)
+    temp = np.vstack(
+        [np.hstack([img_gray, img_blur]), np.hstack([bin, edges])])
+    cv2.imshow(f"Quantidade de objetos: {str(len(contours))}", temp)
+    cv2.waitKey(0)
+
+    imgC2 = img.copy()
+    cv2.imshow("Imagem Original", img)
+    cv2.drawContours(imgC2, contours, -1, (255, 0, 0), 2)
+    escreve(imgC2, str(len(contours))+" objetos encontrados!")
+    cv2.imshow("Resultado", imgC2)
+    cv2.waitKey(0)
+
+    # DRAWCONTOURS -> É usada no OpenCV para desenhar contornos em uma imagem.
+    # image: A imagem onde os contornos serão desenhados. Deve ser uma matriz NumPy (por exemplo, uma imagem carregada com cv2.imread()).
+    # contours: A lista de contornos a serem desenhados. É uma lista de arrays NumPy, onde cada array representa um contorno, geralmente obtido pela função cv2.findContours().
+    # contourIdx: Índice do contorno a ser desenhado. Se definido como -1, todos os contornos serão desenhados.
+    # color: Cor dos contornos. Pode ser especificada como uma tupla (B, G, R) ou um valor escalar inteiro para imagens em tons de cinza.
+    # thickness (opcional): Espessura da linha dos contornos. Se for um valor negativo ou cv2.FILLED, o contorno será preenchido.
+    # lineType (opcional): Tipo de linha a ser usada para desenhar os contornos. Pode ser cv2.LINE_8, cv2.LINE_4, cv2.LINE_AA, etc.
+    # hierarchy (opcional): A hierarquia dos contornos. É um array NumPy que representa a relação de hierarquia entre os contornos. Geralmente é obtido junto com os contornos pela função cv2.findContours().
+    # maxLevel (opcional): O nível máximo da hierarquia a ser desenhado.
+    # offset (opcional): Um deslocamento a ser aplicado a todos os pontos dos contornos.
 
 
 def main():
     image_color = cv2.imread('assets/onePiece.jpg')
     sudoku = cv2.imread('assets/sudoku.PNG', cv2.COLOR_BGR2GRAY)
     river = cv2.imread('assets/river.jpg')
-    road = cv2.imread('assets/road.jpg') 
+    road = cv2.imread('assets/road.jpg')
+    dices = cv2.imread('assets/dices.jpg')
+    dados6 = cv2.imread('assets/dados6.jpeg')
     # cv2.imshow('Source Image', image_color)
 
     print(image_color.shape)
@@ -402,6 +502,9 @@ def main():
 
     # filtroLapraceEdgeDetection(road)
 
-    EdgeDetectionCanny(road)
+    # EdgeDetectionCanny(road)
+
+    identifyAndCountObjects(dados6)
+
 
 main()
